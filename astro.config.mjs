2e77bc6fee3e7a64abd3f8bffd8e4cb4bc238c1b
Victor Avila <vicav99@gmail.com>
2023-01-26 23:49:34 -0700

    initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind(), image()],
 });
