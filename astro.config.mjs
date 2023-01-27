558be4925fc3d3b6e8e222b43840479533004092
Victor Avila <vicav99@gmail.com>
2023-01-27 02:37:03 -0700

    feat: install and setup mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,19 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
+import { remarkReadingTime } from './src/lib/reading-time.ts';
 
 export default defineConfig({
   site: "https://example.com",
+  markdown: {
+    extendDefaultPlugins: true,
+    rehypePlugins: [rehypeAutolinkHeadings],
+    remarkPlugins: [remarkReadingTime],
+    shikiConfig: {
+      theme: "vitesse-dark",
+    },
+  },
   integrations: [mdx(), sitemap(), tailwind(), image()],
 });
