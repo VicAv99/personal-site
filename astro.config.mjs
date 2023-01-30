c34801bc9d680abb780d8d1d5bd308eb0680a244
Victor Avila <vicav99@gmail.com>
2023-01-29 22:07:03 -0700

    chore: add vercel for ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
@@ -20,4 +21,5 @@ export default defineConfig({
     },
   },
   integrations: [mdx(), sitemap(), tailwind(), image(), react()],
+  adapter: vercel(),
 });
