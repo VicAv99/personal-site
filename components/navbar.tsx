7aacbc5f1b7c1faca27f310779aa0eb101049d0f
Victor Avila <vicav99@gmail.com>
2023-08-22 22:43:14 -0700

    style: adjust navbar height

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ import { ThemeToggle } from "./theme-toggle";
 
 export function Navbar() {
   return (
-    <header className="z-30 mb-20 border-b border-zinc-300 px-6 py-2 dark:border-zinc-800 md:mb-24 md:px-16">
+    <header className="z-30 mb-20 border-b border-zinc-300 px-6 py-4 dark:border-zinc-800 md:mb-24 md:px-16">
       <nav className="mx-auto flex max-w-6xl items-center justify-between">
         <Link href="/">
           <h3>NovaChrome</h3>
